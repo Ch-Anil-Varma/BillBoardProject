0ad4776933d58c41fff8c874a61c42c2c89d13ca
Anil Varma Chitturi <anilvarmachitturi29@gmail.com>
2026-02-24 23:04:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/AdGrid";
+
+main().then(() => {
+    console.log("connected to DB");
+}).catch(err =>  {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+    res.send("API Running");
+});
+
+app.listen(8080, () => {
+    console.log("Server listening on port 8080");
+});
